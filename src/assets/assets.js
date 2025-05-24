8c7617200d64eaa51b4b2652d657e38e7764ab90
chetan <mohiturkude98@rediffmail.com>
2025-05-24 09:32:08 +0530

    Images added

---
+++ src/assets/assets.js
@@ -0,0 +1,14 @@
+import banner_bg from './banner_bg.jpg'
+import logo from './logo.jpg'
+import logo1 from './logo1.jpg'
+import logo3 from './logo3.png'
+import fruit_plate from './fruit_plate.png'
+import leaf from './leaf.png'
+export const assets={
+    banner_bg,
+    logo
+    ,logo1,
+    logo3,
+    fruit_plate,
+    leaf
+}
