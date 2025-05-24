546ef5dca4fd7c24c7c5d44a8f06e77bdd9ba1ac
chetan <mohiturkude98@rediffmail.com>
2025-05-24 09:46:21 +0530

    Home.jsx updated

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+import Hero from '../components/Hero'
+
+const Home = () => {
+  return (
+    <div>
+      <Hero/>
+      
+      
+    </div>
+  )
+}
+
+export default Home
