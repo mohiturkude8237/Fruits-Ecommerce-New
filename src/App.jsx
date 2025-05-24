fcc9d413f7f6e6d6d90275d05322cf356d95d3c6
chetan <mohiturkude98@rediffmail.com>
2025-05-24 09:47:17 +0530

    App.jsx Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,53 @@
-import React from 'react'
+// import React from "react";
+// import Navbar from "./components/Navbar";
+// import { assets } from "./assets/assets";
+// import { Route, Routes } from "react-router-dom";
+// import Home from './pages/Home'
+// import Products from './pages/Products'
+// import About from './pages/About'
+// import Shop from './pages/Shop'
+// import Contacts from './pages/Contacts'
+
+
+// const App = () => {
+//   return (
+//     <div>
+//       <Navbar/>
+//       <Routes>
+//         <Route path='/' element={<Home />} />
+//         <Route path='/products' element={<Products />} />
+//         <Route path='/about' element={<About />} />
+//         <Route path='/shop' element={<Shop />} />
+//         <Route path='/contacts' element={<Contacts />} />
+//       </Routes>
+//     </div>
+//   );
+// };
+
+// export default App;
+
+import React from "react";
+import Navbar from "./components/Navbar";
+import { Route, Routes } from "react-router-dom";
+import Home from './pages/Home';
+import Products from './pages/Products';
+import About from './pages/About';
+import Shop from './pages/Shop';
+import Contacts from './pages/Contacts';
 
 const App = () => {
   return (
-    <div className='text-3xl bg-amber-400 text-center'>
-      This is fruit sale project.
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/products' element={<Products />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/shop' element={<Shop />} />
+        <Route path='/contacts' element={<Contacts />} />
+      </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
