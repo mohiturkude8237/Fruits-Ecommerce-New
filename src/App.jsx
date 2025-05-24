66942c182707fc9c78c4850a097abae852d1fd84
chetan <mohiturkude98@rediffmail.com>
2025-05-24 09:24:17 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='text-3xl bg-amber-400 text-center'>
+      This is fruit sale project.
+    </div>
+  )
+}
+
+export default App
